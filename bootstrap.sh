a5f348bbcf4bc521fc915a72edc788348a5a4345
Jay Monga <jaymonga16@berkeley.edu>
2021-02-04 16:59:48 -0800

    Fix key for ROS packages
    Key needs to be updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,7 +45,8 @@ message "Adding ros repositories..."
 if ! grep "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" /etc/apt/sources.list.d/ros-latest.list
 	then sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 fi
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 # removed, headmelted build of code-oss does not run on pi zero
 # message "Adding codebuilds key..."
